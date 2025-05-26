7083c6a0f0724cb57e4862725940247f93848364
rakeshkumar7416 <rakesh.kumar@provility.com>
2025-05-26 12:38:29 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import "./styles/main.scss";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-import './App.css';
+import SignInForm from "./components/SignInForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to code Driver !!!!!!</h1>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/login" element={<SignInForm />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/SignInForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./SignInForm.scss";
+
+export default function SignInForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    return (
+        <div className="d-flex align-items-center justify-content-center min-vh-100 bg-cover w-100">
+        <div className="login-box card p-4 shadow-sm">
+                <div className="brand-logo mb-3"></div>
+                <h2 className="text-center fw-bolder mb-4">Sign On</h2>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label fw-semibold">Email<span
+                        className="text-danger">*</span></label>
+                    <input type="email" className="form-control" id="email" placeholder="Enter email" value={email}
+                           onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label fw-semibold">Password<span
+                        className="text-danger">*</span></label>
+                    <input type="password" className="form-control" id="password" placeholder="Password"
+                           value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <button type="submit" className="btn btn-login w-100">Sign On</button>
+                <div className="text-center mt-3 ">
+                    <a href="/login" className="text-dark text-decoration-none">Forgot password</a>
+                    <p >Don't have an account? <a className="primary-color fw-bold  text-decoration-none" href="/login">Sign Up</a></p>
+                </div>
+            </form>
+        </div>
+        </div>
+    );
+}
